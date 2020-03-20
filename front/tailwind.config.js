f75492428c8055c059dce65151db1932c226acde
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-03-20 20:54:46 +0100

    Add tailwindcss children (#13)

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -8,6 +8,10 @@ module.exports = {
       }),
     },
   },
-  variants: {},
-  plugins: [],
+  variants: {
+    display: ['children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
+  },
+  plugins: [
+    require('tailwindcss-children'),
+  ],
 }
