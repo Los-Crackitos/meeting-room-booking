a0393639a84ddb2c5edf396d45648ad1bbc65155
Jérôme Pogeant <p-jerome@hotmail.fr>
2020-03-23 09:25:31 +0100

    Revert "Add tailwindcss children (#13)" (#14)
    This reverts commit f75492428c8055c059dce65151db1932c226acde.

--- front/tailwind.config.js
+++ front/tailwind.config.js
@@ -8,10 +8,6 @@ module.exports = {
       }),
     },
   },
-  variants: {
-    display: ['children', 'default', 'children-first', 'children-last', 'children-odd', 'children-even', 'children-not-first', 'children-hover', 'hover', 'children-focus', 'focus', 'children-focus-within', 'focus-within', 'children-active', 'active', 'children-visited', 'visited', 'children-disabled', 'disabled', 'responsive'],
-  },
-  plugins: [
-    require('tailwindcss-children'),
-  ],
+  variants: {},
+  plugins: [],
 }
